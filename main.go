da73ae414df258af85bbfa85c003d9e9e6e0535c
Baksman <Ibrahimshehu73700@gmail.com>
2020-03-09 15:56:54 +0100

    first implementation of blockchain

--- main.go
+++ main.go
@@ -1,34 +1,56 @@
 package main
 
 import (
-	"os"
-	"compress/gzip"
-	"io"
+	"bytes"
+	"crypto/sha256"
+	"fmt"
 )
 
-func main() {
-	for _, file := range os.Args[1:] {
-		compress(file)
-	}
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
 }
 
-func compress(filename string) error {
-	in, err := os.Open(filename)
-	if err != nil {
-		return err
+func (b *Block) DeriveHash() {
+	//join two slice of bytes
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{GenesisBlock()}}
+}
+
+func GenesisBlock() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("First blockchain after genesis")
+	chain.AddBlock("second blockchain after genesis")
+	chain.AddBlock("third block after genesis block")
+
+	for key, val := range chain.blocks {
+		fmt.Printf("the number %v  is %x \n", key, val)
 	}
-	defer in.Close()
-	out,err := os.Create(filename + ".gz")
-	if err!= nil{
-		return err
-	}
-	defer out.Close()
-
-
-	gzout := gzip.NewWriter(out)
-	_,err = io.Copy(gzout,in)
-	gzout.Close()
-
-	return err
-
 }
